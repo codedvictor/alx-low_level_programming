a291b3fe0f76839f6c76a4926146dab887b91f07
Ige Victor <victorkoladeige@gmail.com>
2023-02-16 17:36:03 -0800

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main is the input
+ *
+ *return is the output
+ */
+int main(void)
+{	
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
+
+	return (1);
+}
