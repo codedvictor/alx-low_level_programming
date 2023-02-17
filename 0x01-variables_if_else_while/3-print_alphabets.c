4d94c2a57ffccf12a0c0043a51a4f9cc03cdf19e
Ige Victor <victorkoladeige@gmail.com>
2023-02-17 11:21:45 -0800

    betty style

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,11 +12,12 @@ int main(void)
 {
 	char alpha;
 	char beta;
-	for (alpha = 'a'; alpha <= 'z'; alpha += 1)
+
+		for (alpha = 'a'; alpha <= 'z'; alpha += 1)
 	{
 	     putchar(alpha);
 	};
-	for (beta = 'A'; beta <= 'Z'; beta += 1)
+		for (beta = 'A'; beta <= 'Z'; beta += 1)
 	{
 	     putchar(beta);
 	};
