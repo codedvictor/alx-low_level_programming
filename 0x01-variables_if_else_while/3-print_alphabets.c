4ea4cc9bf96de1f160273818b6b8dd62c9505c4a
Ige Victor <victorkoladeige@gmail.com>
2023-02-17 10:59:19 -0800

    aA-zZ

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char alpha;
+	     char beta;
+	     for (alpha = 'a'; alpha <= 'z'; alpha +=1)
+		     putchar(alpha);
+	for (beta = 'A'; beta <- 'Z'; beta +=1)
+		putchar(beta);
+	putchar('\n');
+
+	return (0);
+}
