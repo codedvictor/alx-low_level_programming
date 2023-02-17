d123c04ff5b7d103dc947366dcc4ca32acd63e86
Ige Victor <victorkoladeige@gmail.com>
2023-02-17 12:16:35 -0800

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+/* more headers goes there */
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+        int i;
+
+        for (i = 0; i < 10; i += 1)
+        {
+                putchar(i);
+        };
+                putchar('\n');
+        return (0);
+}
