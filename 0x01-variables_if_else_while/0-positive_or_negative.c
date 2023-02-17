4a3bf7ee9526b533ca3eb40c09fd2edda688d204
Ige Victor <victorkoladeige@gmail.com>
2023-02-17 09:20:42 -0800

    betty commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,6 +3,7 @@
 /* more headers goes there */
 #include <stdio.h>
 /* betty style doc for function main goes there */
+/* main is the entry point */
 int main(void)
 {
 	int n;
@@ -12,7 +13,7 @@ int main(void)
 	/* your code goes there */
 	if (n > 0)
 		printf("%d is positive\n", n);
-	else if(n==0)
+	else if(n == 0)
 		printf("%d is zero\n", n);
 	else
 		printf("%d is negative\n", n);
