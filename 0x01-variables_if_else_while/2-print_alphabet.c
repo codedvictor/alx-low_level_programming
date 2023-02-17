b555a839632e97e9ebb9f35587300012bda1e9cd
Ige Victor <victorkoladeige@gmail.com>
2023-02-17 10:29:00 -0800

    correction

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,10 +1,17 @@
+/* more headers goes there */
 #include <stdio.h>
 
-int main()
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main (void)
 {
 	char alpha;
 
-	for (alpha = 'a'; alpha <= 'z'; alpha += 1)
+	for ( alpha = 'a'; alpha <= 'z '; alpha += 1)
 	{
 		putchar(alpha);
 	};
