1ee2f871b4dfc8a40d0242f32b4cc2816f82ec5f
Ige Victor <victorkoladeige@gmail.com>
2023-02-17 11:18:31 -0800

    betty update

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,18 +1,26 @@
+/* more headers goes there */
 #include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
 	char alpha;
 	char beta;
-	     for (alpha = 'a'; alpha <= 'z'; alpha +=1)
-	     {
-		     putchar(alpha);
-		};
-	     for (beta = 'A'; beta <= 'Z'; beta +=1)
-	     {
-		     putchar(beta);
-	     };
-		putchar('\n');
+	for (alpha = 'a'; alpha <= 'z'; alpha += 1)
+	{
+	     putchar(alpha);
+	};
+	for (beta = 'A'; beta <= 'Z'; beta += 1)
+	{
+	     putchar(beta);
+	};
+	putchar('\n');
 
 	return (0);
 }
