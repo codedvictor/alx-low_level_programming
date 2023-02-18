8c000bf1e5f483cf870f9564d902089c2c83f778
Ige Victor <victorkoladeige@gmail.com>
2023-02-17 16:15:41 -0800

    102-print_comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (j = 0; j <= 100; j += 1)
+	{
+		for (i = 0; i <= 100; i += 1)
+		{
+			if  (j > i)
+			{
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				putchar(' ');
+				putchar((i / 10) + 48);
+				putchar((i / 10) + 48);
+				if (j != 98 || i != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
