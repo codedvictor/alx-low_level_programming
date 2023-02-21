3d92706e6c014cd391cb5c738f79c3fc6d38618e
Ige Victor <victorkoladeige@gmail.com>
2023-02-21 11:03:56 -0800

    betty style

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,14 @@
 #include "main.h"
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
 /**
- * main - check the code
- * print_alphabet - into lower case
- * Return: Always 0.
+ * print_alphabet - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 
 void print_alphabet(void)
-	/*printing lowercase */
 {
 	int i;
 
