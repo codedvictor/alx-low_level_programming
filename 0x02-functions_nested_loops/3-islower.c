b8f68ccd93783c75471ddaa63ae2910d1988f698
Ige Victor <victorkoladeige@gmail.com>
2023-02-21 11:59:25 -0800

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,9 +4,9 @@
 /* betty style doc for function main goes there */
 /**
  * _islower -Entry point
- * int c - is the integer
+ * c - islower
  * Return: 1 (Success)
- * Return: 0 (failed)
+ *
  */
 
 int _islower(int c)
