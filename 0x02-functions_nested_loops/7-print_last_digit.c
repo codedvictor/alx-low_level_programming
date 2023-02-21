d94d4dabddfdd381a12e9e7ec35c31334e3c2eeb
Ige Victor <victorkoladeige@gmail.com>
2023-02-21 14:01:44 -0800

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * print_last_digit -Entry point
+ *
+ * @n: is the last number to compute
+ *
+ * Return: 1 (Success) otherwise 0
+ */
+
+int print_last_digit(int n)
+{
+	int i;
+
+	i = n % 10;
+	
+	if (i < 0)
+	{
+		i = i * (-1);	
+	}
+	_putchar (i + '0');
+       
+	return (i);
+}
