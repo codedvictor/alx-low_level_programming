d48292b2283d62864488061794c09dea27dd3ad9
Ige Victor <victorkoladeige@gmail.com>
2023-02-21 11:14:50 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,14 +10,13 @@
 
 void print_alphabet_x10(void)
 {
-	int i;
-
-	for (i = 'a'; i <= 'z'; i += 1)
+	int n;
+	for (n = 0; n <= 10; n += 1)
 	{
-		int n;
-		for (n = 0; n <= 10; n += 1)
+		int i;
+		for (i = 'a'; i <= 'z'; i += 1)
 		{
-			_putchar(i * n-1);
+			_putchar(i);
 		}
 	}
 	_putchar('\n');
