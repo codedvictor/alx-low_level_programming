a2fa205dc05e7e20b1166e1126c402359dbe47e3
Ige Victor <victorkoladeige@gmail.com>
2023-02-21 13:05:31 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,14 +14,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		return (+1);
+		_putchar ('+');
+		return (1);
 	}
 	else if (n < 0)
 	{
-		return (-1);
+		_putchar ('-');
+		return (1);
 	} else
 	{
-		return (00);
+		_putchar (0);
+		return (0);
 	}
 
 }
