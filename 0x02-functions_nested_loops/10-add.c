50365f930ddec88c63d5bdd163af687a9b447414
Ige Victor <victorkoladeige@gmail.com>
2023-02-21 15:35:21 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * times_table - Entry point
+ *
+ * @i & j: is the hour and minutes of the day
+ *
+ * Return: 1 (Success) otherwise 0
+ */
+
+int add(int i, int j)
+{
+	int sum;
+	sum = i + j;
+	return sum;
+}
