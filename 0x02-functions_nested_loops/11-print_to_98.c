252a6f60637b979c9d2adda39dcf3ca21ec6a748
Ige Victor <victorkoladeige@gmail.com>
2023-02-21 15:52:24 -0800

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * print_to_98 - Entry point
+ *
+ * @n: is the lowest integer
+ *
+ * Return: 1 (Success) otherwise 0
+ */
+
+void print_to_98(int n)
+{
+	int i
+
+	for (i = n; i <= 98; i += 1)
+	{
+		if (i < 10)
+		{
+			_putchar (i + '0');
+			_putchar (',');
+			_putchar (' ');
+		}
+		else if (i >= 10) 
+		{
+			_putchar ((z / 10) + '0');
+			_putchar ((z % 10) + '0');
+			_putchar (',');
+			_putchar (' ');
+		}
+	}
+	_putchar ('\n');
+}
