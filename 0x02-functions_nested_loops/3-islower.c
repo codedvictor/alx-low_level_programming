5c09b9b53d9267881c096264db07d2c3fdaf48bc
Ige Victor <victorkoladeige@gmail.com>
2023-02-21 11:34:17 -0800

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * print_alphabet_x10 -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _islower(int c)
+{
+	int c;
+
+	if (c == islower)
+	{
+		return 1;
+	}else
+		return 0;
+
+}
