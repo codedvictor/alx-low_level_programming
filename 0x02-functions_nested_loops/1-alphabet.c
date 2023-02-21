60679370a367826c3dfec4da9475259a67ef10e5
Ige Victor <victorkoladeige@gmail.com>
2023-02-21 10:02:38 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void);
+
+int main(void)
+{
+    char alpha = print_alphabet();
+    putchar('alpha');
+    putchar('\n')
+    return (0);
+}
+
+void print_alphabet(void){
+	char low;
+	char i;
+
+	low = tolower(i);
+	return low;
+}
