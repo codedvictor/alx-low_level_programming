90b8022f73250a3c84ac9e1dca96503d78864a57
Ige Victor <victorkoladeige@gmail.com>
2023-02-21 11:19:37 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,7 +3,7 @@
 
 /* betty style doc for function main goes there */
 /**
- * print_alphabet - Entry point
+ * print_alphabet_x10 -Entry point
  *
  * Return: Always 0 (Success)
  */
@@ -11,9 +11,11 @@
 void print_alphabet_x10(void)
 {
 	int n;
-	for (n = 0; n <= 10; n += 1)
+
+	for (n = 0; n < 10; n += 1)
 	{
 		int i;
+
 		for (i = 'a'; i <= 'z'; i += 1)
 		{
 			_putchar(i);
