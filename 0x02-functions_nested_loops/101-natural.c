2bde72396d2f28a106cc15e4103130998d2e294a
Ige Victor <victorkoladeige@gmail.com>
2023-02-21 18:35:25 -0800

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * print_last_digit -Entry point
+ *
+ * @n: is the last number to compute
+ *
+ * Return: 1 (Success) otherwise 0
+ */
+
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 1024; i += 3)
+	{
+		for (j = 0; j < 1024; j += 5)
+		{
+			k = i * j;
+			
+			k += k;
+		}
+		printf("%d\n", k);
+
+	}
+}
