d991d8dc75577e5d8e7dcf9c2e8d2e3b3980a82e
Ige Victor <victorkoladeige@gmail.com>
2023-02-22 05:03:55 -0800

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,14 +2,13 @@
 
 /**
 * main - tests function that prints if integer is positive or negative
-* i positive_or _negative: is the function to perform sign check
 * Return: 0
 */
 
 int main(void)
 {
 	int i;
-
+	
 	i = 98;
 	positive_or_negative(i);
 
