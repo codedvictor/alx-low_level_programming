f948e03c72eee3a567e175d235971638ecc7d16d
Ige Victor <victorkoladeige@gmail.com>
2023-02-21 17:05:11 -0800

    11-print_to_98.c using print

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,21 +7,21 @@
  * print_to_98 - Entry point
  *
  * @n: is the lowest integer
- * @i: one of the integer
- * @j: the second one
+ * 
  * Return: 1 (Success) otherwise 0
  */
 
 void print_to_98(int n)
 {
 	int i, j;
+
 	if (n <= 98)
 	{
 		for (i = n; i <= 98; i += 1)
 		{
 			if (i != 98)
 			{
-				printf("%d,", i);
+				printf("%d, ", i);
 			}
 			else if (i == 98)
 			{
@@ -35,7 +35,7 @@ void print_to_98(int n)
 		{
 			if (j != 98)
 			{
-				printf("%d,", j);
+				printf("%d, ", j);
 			}
 			else if (j == 98)
 			{
