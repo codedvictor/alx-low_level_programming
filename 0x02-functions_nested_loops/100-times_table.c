391ab1884576b6406ac9ab55d4ec6476a9563d85
Ige Victor <victorkoladeige@gmail.com>
2023-02-21 17:31:38 -0800

    100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,57 @@
+#include "main.h"
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * print_times_table - Entry point
+ *
+ * @n: is the integer
+ *
+ * Return: 1 (Success) otherwise 0
+ */
+
+void print_times_table(int n)
+{
+	if (n >= 0 && n <= 14)
+	{
+		int i, j;
+
+		for (i = 0; i <= n; i += 1)
+		{
+			for (j = 0; j <= n; j += 1)
+			{
+				int z = i * j;
+				if (z > 99)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar((z / 100) + '0');
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+				else if (z > 9)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar(32);
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+				else
+				{
+					if (j != 0)
+					{
+						_putchar(',');
+						_putchar(32);
+						_putchar(32);
+						_putchar(32);
+					}
+					_putchar(z + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
+
