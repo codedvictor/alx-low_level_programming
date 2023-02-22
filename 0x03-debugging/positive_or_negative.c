4592bf8460e59f60fb8dc6824569591e8a46e1ab
Ige Victor <victorkoladeige@gmail.com>
2023-02-22 05:47:40 -0800

    positive_or_negative.c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -4,23 +4,20 @@
 
 #include <stdio.h>
 /* betty style doc for function main goes there */
-/**
+/*
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+void positive_or_negative(int i)
 {
-	int n;
-
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	i = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n == 0)
-		printf("%d is zero\n", n);
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i == 0)
+		printf("%d is zero\n", i);
 	else
-		printf("%d is negative\n", n);
-	return (0);
+		printf("%d is negative\n", i);
 }
