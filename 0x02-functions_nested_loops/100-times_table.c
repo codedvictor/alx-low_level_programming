970edf331b5cbd4a9e0f558cab45a201d9051edb
Ige Victor <victorkoladeige@gmail.com>
2023-02-21 18:02:20 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -20,17 +20,11 @@ void print_times_table(int n)
 				}
 				else if (k < 10 && j != 0)
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
-				}
+					printf(",   ");
+					_putchar(k + '0');						}
 				else if (k >= 10 && k < 100)
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
+					printf(",  ");
 					_putchar((k / 10) + '0');
 					_putchar((k % 10) + '0');
 				}
