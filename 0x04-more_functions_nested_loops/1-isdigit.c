3b8171c4bfa56e05bb9084d870f8854cdf63048c
Ige Victor <victorkoladeige@gmail.com>
2023-02-23 06:35:11 -0800

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isdigit - main entry
+ * @c: is the number to check
+ * Return: 1 success
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -8,5 +8,6 @@ int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
 int _isupper(int c);
+int _isdigit(int c);
 
 #endif /* MAIN_H */
