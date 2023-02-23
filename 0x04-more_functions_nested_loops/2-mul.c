3f0b71e83a7ecd249ac9e0dde11d2a845af3c59f
Ige Victor <victorkoladeige@gmail.com>
2023-02-23 10:22:45 -0800

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isdigit - Check if paramter c represents an digit.
+ * @a: first integer
+ * @b: second integer
+ * @product: the multiply
+ * Return: Return 1 if it is a digit otherwise return 0.
+ */
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+	return (product);
+}
