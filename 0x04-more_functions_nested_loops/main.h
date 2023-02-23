5351f4e8fb149f152299551c15ee404d79e8a8d3
Ige Victor <victorkoladeige@gmail.com>
2023-02-23 12:10:18 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -11,7 +11,7 @@ int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
-char _putchar(char c);
+char _putchar(int c);
 void print_most_numbers(void);
 void more_numbers(void);
 
