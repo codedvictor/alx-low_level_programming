5788bf7e046066502e89648f427ee8bad0d54190
Ige Victor <victorkoladeige@gmail.com>
2023-02-23 10:34:20 -0800

    mul c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,12 +1,11 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * _isdigit - Check if paramter c represents an digit.
- * @a: first integer
- * @b: second integer
- * Return: 0
+ * mul - returns the product of its parameters
+ * @a: int type number
+ * @b: int type number
+ * Return:0
  */
+
 int mul(int a, int b)
 {
 	return (a * b);
