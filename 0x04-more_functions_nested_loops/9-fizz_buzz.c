c393585d784227d0f05d45a61562c89cd75ccf27
Ige Victor <victorkoladeige@gmail.com>
2023-02-23 15:33:51 -0800

    9-fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,7 +1,9 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * main - entry point
+ *
  * Return:0 success
  */
 
@@ -13,23 +15,23 @@ int main(void)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
                 {
-                        printf("FizzBuzz ");
+                        printf("FizzBuzz");
                 }
 		else if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else
 		{
-		printf("%d ", i);
+		printf("%d", i);
 		}
-		if (i == 100)
+		if (i != 100)
 		{
-			printf("Buzz");
+			printf(" ");
 		}
 	}
 	printf("\n");
