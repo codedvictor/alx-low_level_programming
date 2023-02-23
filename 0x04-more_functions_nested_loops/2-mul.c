00e0d43373638b8ef601da9dc6286ced87cb0dc8
Ige Victor <victorkoladeige@gmail.com>
2023-02-23 10:29:10 -0800

    multiplication

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -5,7 +5,6 @@
  * _isdigit - Check if paramter c represents an digit.
  * @a: first integer
  * @b: second integer
- * @product: the multiply
  * Return: Return 1 if it is a digit otherwise return 0.
  */
 int mul(int a, int b)
