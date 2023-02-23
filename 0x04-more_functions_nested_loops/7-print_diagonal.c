3b4a4a07cca445f1db0c184bfd768b5ee694b7c3
Ige Victor <victorkoladeige@gmail.com>
2023-02-23 14:10:45 -0800

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -13,15 +13,13 @@ void print_diagonal(int n)
 	if (n <= 0)
 	{
 		_putchar ('\n');
-		break;
 	}
 	else
 	{
 		for (i = 1; i <= n; i += 1)
 			{
-			_putchar ('\');
+			_putchar (92);
 			}
 		_putchar ('\n');
-		break;
 	}
 }
