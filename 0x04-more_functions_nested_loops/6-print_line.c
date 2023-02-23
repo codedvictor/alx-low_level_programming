c7445f5a2b38d1cb0b132244edfd0164f8745578
Ige Victor <victorkoladeige@gmail.com>
2023-02-23 13:04:55 -0800

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,15 +10,15 @@ void print_line(int n)
 	int i;
 
 	if (n <= 0)
-		{
-			_putchar ('\n');
-		}
-		else
-		{
-			for (i = 0; i <= n; i += 1)
-				{
-				_putchar ('_');
-				}
-			_putchar ('\n');
-		}
+	{
+		_putchar ('\n');
+	}
+	else
+	{
+		for (i = 1; i <= n; i += 1)
+			{
+			_putchar ('_');
+			}
+		_putchar ('\n');
+	}
 }
