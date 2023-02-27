eba0bd61176a0cf5e9bbc68fe36c55f0a6251fff
Ige Victor <victorkoladeige@gmail.com>
2023-02-27 13:14:13 -0800

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - reset the code
+ * @n: change the value
+ * Return: Always 0.
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -20,5 +20,6 @@ void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
 
 #endif /* MAIN_H */
