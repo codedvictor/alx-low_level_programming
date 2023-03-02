328e93e8166e8cfa33c31a77ec7228f3dd8fedfa
Ige Victor <victorkoladeige@gmail.com>
2023-03-02 07:10:17 -0800

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+
+/**
+  * _strcmp - Compares two strings
+  * @s1: The first string
+  * @s2: The second string
+  *
+  * Return: int value
+  */
+int _strcmp(char *s1, char *s2)
+{
+	int x, y, z, j, lim;
+
+	for (x = 0; s1[x]; x++)
+	{
+		for (y = 0; s2[y]; y++)
+		{
+			if (x <= y)
+			{
+				lim = x;
+			}
+			else
+			{
+				lim = y;
+			}
+		}
+	}
+	while (z <= lim)
+	{
+		if (s1[z] == s2[z])
+		{
+			z++;
+			continue;
+		}
+		else
+		{
+			j = s1[z] - s2[z];
+			break;
+		}
+
+		z++;
+	}
+	
+	return (j);
+}
