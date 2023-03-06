838f469336b0d8a2357e7138889106f9175dcd92
Ige Victor <victorkoladeige@gmail.com>
2023-03-06 11:54:42 -0800

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memset - fills memory
+ * @s: the address to print
+ * @b: the size of the memory to print
+ * @n: the varible
+ * Return: Nothing.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	b = n;
+	s = &b;
+
+	return (s);	
+}
