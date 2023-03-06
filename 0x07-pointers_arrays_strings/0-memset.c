680736b5be3aaf4fac4edcc1cfa78d87413b8055
Ige Victor <victorkoladeige@gmail.com>
2023-03-06 12:11:59 -0800

    0-memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,8 +6,9 @@
  * @s: the address to print
  * @b: the size of the memory to print
  * @n: the varible
- * Return: Nothing.
+ * Return: value of s
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int x;
