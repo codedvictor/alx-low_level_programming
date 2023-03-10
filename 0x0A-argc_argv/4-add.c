20069ee98d2517c6fea006edd2251349f4b7dcf0
Ige Victor <victorkoladeige@gmail.com>
2023-03-10 06:51:45 -0800

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,21 +13,22 @@ int main(int argc, char *argv[])
 	int i;
 	int add = 0;
 
-	if (argc == 1)
+	if (argc < 1)
 	{
 		printf("0\n");
 	}
-	else if (argv[i] = '\0')
+	for (i = 1; i < argc; i++)
 	{
-		printf("Error\n");
-	}
-	else
-	{
-		for (i = 1; i < argc; i++)
+		if (!atoi(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
 		{
 			add += atoi(argv[i]);
 		}
-		printf("%d\n", add);
 	}
+	printf("%d\n", add);
 return (0);
 }
