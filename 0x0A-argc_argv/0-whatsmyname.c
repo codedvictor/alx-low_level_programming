3cf7b8731ab0ab40190d94c711d5112dcf1c5f70
Ige Victor <victorkoladeige@gmail.com>
2023-03-10 03:57:40 -0800

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - prints its name, followed by a new line.
- * @argc : array of commands count
- * @argv : array of commands
- * return : 0
+ * main - prints its name with new line
+ * @argc: array of commands count
+ * @argv: array of commands
+ * return: 0
  */
 
 int main(int argc, char *argv[])
