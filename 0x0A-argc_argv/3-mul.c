59026bf93dae26d63b59e77f65c626a857b5e95e
Ige Victor <victorkoladeige@gmail.com>
2023-03-10 06:08:32 -0800

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/**
+ * main - prints the number of argument
+ * @argc: number of arguments typed
+ * @argv: array pointing to arguements
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int mul = 1;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			mul *= atoi(argv[i]);
+		}
+		printf("%d\n", mul);
+	}
+return (0);
+}
