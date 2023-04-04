fcb74b84a66039815fae66779a8976c1fd77d864
Ige Victor <victorkoladeige@gmail.com>
2023-04-04 04:19:35 -0700

    task 1

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * print_listint- print all the elements
+ * @h: element to print
+ *
+ * Return: The number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int i = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
