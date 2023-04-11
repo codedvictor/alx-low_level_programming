1e69db69da7f144d3fdcc921baeb28f270f6b71c
Ige Victor <victorkoladeige@gmail.com>
2023-04-11 14:59:07 -0700

    task 5 completed

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -12,16 +12,16 @@
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int flip;
-	unsigned int buff;
+	unsigned long int flip = 0;
+	unsigned int buff = 0;
 
 	flip = n ^ m;
 
-	while (flip != 0)
+	while (flip)
 	{
 		if (flip & 1ul)
 			buff++;
-		flip = flip >> 1;
+		flip >>= 1;
 	}
 
 	return (buff);
