91e1b35e87577f6e15d0ad00217cdd0580bdb9fa
Ige Victor <victorkoladeige@gmail.com>
2023-04-10 18:22:40 -0700

    2n tak

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,27 +10,28 @@
  *
  * Return: actual number of letters read, 0 if end of file
  */
+
 int create_file(const char *filename, char *text_content)
 {
-	int ofile, len = 0, nfile = 0;
+	int ofile;
+	int nFile = 0, len = 0;
 	char *buff;
 
 	if (filename == NULL)
 		return (-1);
 
-	ofile = open(filename, O_RDWR | O_CREAT | O_TRUNC);
+	ofile = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (ofile == -1)
 		return (-1);
 
-	if (text_content)
+	if (text_content != NULL)
 	{
 		for (len = 0, buff = text_content; *buff; buff++)
 			len++;
-
-		nfile = write(ofile, text_content, len);
+		nFile = write(ofile, text_content, len);
 	}
-	if (!(len == nfile) || (close(ofile) == -1))
-		return (-1);
 
+	if (close(ofile) == -1 || len != nFile)
+		return (-1);
 	return (1);
 }
