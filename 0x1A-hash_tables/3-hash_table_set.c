1f5d0c4a1432ede3bc8df88f8fdf705510c68630
Ige Victor <victorkoladeige@gmail.com>
2023-06-13 01:45:22 -0700

    betty correction

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -2,12 +2,10 @@
 
 /**
  * create_and_add_node - malloc, set values, and insert node into hash table
- * 
  * @ht: hash table
  * @key: key; can't be empty string
  * @value: value
- *  * @idx: index to insert in at hash table
- * 
+ * @idx: index to insert in at hash table
  * Return: 1 if success, 0 if fail
  */
 
@@ -51,20 +49,13 @@ int create_and_add_node(hash_table_t *ht, const char *key, const char *value,
 
 /**
  * hash_table_set - add element to hash table
- * 
  * @ht: hash table
  * @key: key; can't be empty string
  * @value: value
- * 
  * Return: 1 if success, 0 if fail
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	/* get index */
-	/* if key already exists, update value and return */
-	/* else create node */
-	/* set ht idx ptr to node; else add node to front if collision */
-
 	unsigned long int id;
 	hash_node_t *nod = NULL;
 	char *y;
