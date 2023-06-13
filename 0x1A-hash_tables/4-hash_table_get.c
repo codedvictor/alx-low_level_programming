9342ab720bd5276b93fe51e147674dc2f7802ed4
Ige Victor <victorkoladeige@gmail.com>
2023-06-13 01:52:03 -0700

    4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -2,10 +2,8 @@
 
 /**
  * hash_table_get - given key, get value
- * 
  * @ht: hash table
  * @key: key
- *
  * Return: value; or NULL if not found
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
