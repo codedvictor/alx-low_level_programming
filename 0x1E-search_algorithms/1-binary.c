b5939ed533d4025cea11844feead69ed3f895465
Ige Victor Kolade <victorkoladeige@gmail.com>
2023-09-12 11:24:15 -0700

    bug fixing

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -14,7 +14,7 @@ int binary_search(int *array, size_t size, int value)
 {
 	size_t x, l, r;
 
-	if (array == NULL)
+	if (array == NULL || size == 0)
 		return (-1);
 
 	for (l = 0, r = size - 1; r >= l;)
