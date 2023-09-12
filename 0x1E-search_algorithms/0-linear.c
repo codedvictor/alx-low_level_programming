c41bd04e551e5f31020aef0c2d4f2c8ff0633c6e
Ige Victor Kolade <victorkoladeige@gmail.com>
2023-09-12 07:22:46 -0700

    linear searching

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -21,7 +21,7 @@ int linear_search(int *array, size_t size, int value)
 	for (x = 0; x < size; x++)
 	{
 		printf("Value checked array[%ld] = [%d]\n", x, array[x]);
-		if (value == array[x])
+		if (array[x] = value)
 		{
 			return (x);
 		}
