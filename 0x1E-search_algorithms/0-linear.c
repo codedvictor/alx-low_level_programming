cda1fe378f86a8fa6a9e91001755c6d28279f41a
Ige Victor Kolade <victorkoladeige@gmail.com>
2023-09-12 09:43:56 -0700

    bug fixing on the fnction

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,7 +14,7 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t x;
 
-	if (array == NULL || !value)
+	if (array == NULL)
 		return (-1);
 	for (x = 0; x < size; x++)
 	{
--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -14,7 +14,7 @@ int binary_search(int *array, size_t size, int value)
 {
 	size_t x, l, r;
 
-	if (array == NULL && !value)
+	if (array == NULL)
 		return (-1);
 
 	for (l = 0, r = size - 1; r >= l;)
